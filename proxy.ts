9e35655318ff5c4517c03f101d45bab96c5a9df2
Manan <manan@appperfect.com>
2026-03-23 11:57:28 +0530

    removed unused imports

--- proxy.ts
+++ proxy.ts
@@ -1,5 +1,3 @@
-import { matchesMiddleware } from "next/dist/shared/lib/router/router";
-import { Tiro_Kannada } from "next/font/google";
 import { NextRequest, NextResponse } from "next/server";
 
 export function proxy(request: NextRequest){ 
