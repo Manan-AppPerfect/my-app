4a691c1e0683e7f516f2343798bddafc1dcdf622
Manan <manan@appperfect.com>
2026-03-24 17:10:56 +0530

    added store for adding redux later

---
+++ store/cartSlice.ts
@@ -0,0 +1,38 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+type Product = {
+  id: number;
+  title: string;
+  price: number;
+};
+
+type CartState = {
+  items: Product[];
+};
+
+const initialState: CartState = {
+  items: [],
+};
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState,
+  reducers: {
+    addToCart: (state, action: PayloadAction<Product>) => {
+      state.items.push(action.payload);
+    },
+
+    removeFromCart: (state, action: PayloadAction<number>) => {
+      state.items = state.items.filter(
+        (item) => item.id !== action.payload
+      );
+    },
+
+    clearCart: (state) => {
+      state.items = [];
+    },
+  },
+});
+
+export const { addToCart, removeFromCart, clearCart } = cartSlice.actions;
+export default cartSlice.reducer;
