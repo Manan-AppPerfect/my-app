fb767de1e3a0a36865957c071c0f3091575a35f5
Manan <manan@appperfect.com>
2026-03-25 14:29:55 +0530

    added localStorage to the store for saving data on refresh

--- store/store.ts
+++ store/store.ts
@@ -2,12 +2,48 @@ import { configureStore } from "@reduxjs/toolkit";
 import cartReducer from "./cartSlice";
 import productReducer from "./productSlice";
 
+const loadCart = () => {
+  if (typeof window === "undefined") {
+    return { items: [], totalPrice: 0 };
+  }
+
+  try {
+    const data = localStorage.getItem("cart");
+
+    if (!data) {
+      return { items: [], totalPrice: 0 };
+    }
+
+    const parsed = JSON.parse(data);
+
+    return {
+      items: parsed.items ?? [],
+      totalPrice: parsed.totalPrice ?? 0,
+    };
+  } catch {
+    return { items: [], totalPrice: 0 };
+  }
+};
+
+const saveCart = (state: RootState) => {
+    if(typeof window === "undefined") return;
+
+    localStorage.setItem("cart", JSON.stringify(state.cart));
+}
+
 export const store = configureStore({
     reducer:{
         cart: cartReducer,
         product: productReducer,
     },
-})
+    preloadedState: {
+        cart: loadCart(),
+    }
+});
+
+store.subscribe(() => {
+    saveCart(store.getState());
+});
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
