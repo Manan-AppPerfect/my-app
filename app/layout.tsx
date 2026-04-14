10e82c49bedf1b3facc25facd98c407d241fd3b4
Manan <manan@appperfect.com>
2026-04-14 12:28:31 +0530

    removed unused imports

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./styles/globals.css";
 import Navbar from "@/components/navbar/Navbar";
 import { Provider } from "react-redux";
 import { store } from "@/store/store";
